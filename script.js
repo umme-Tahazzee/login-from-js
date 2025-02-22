5e5fd34ae15966cd629d285d319cb4cb898ff17b
umme-Tahazzee <tahazzeekaria1@gmail.com>
2025-02-22 20:47:11 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+//form submit reloading the page
+
+
+//step-1 event handler
+document.getElementById("btn-login")
+                .addEventListener('click', function(event){
+                  event.preventDefault(); 
+            
+                 //step-2 : get the phone number
+              const phoneNumber = document.getElementById('phone-number').value
+              console.log(phoneNumber);
+              
+                
+ })
+
+
+
+
+
