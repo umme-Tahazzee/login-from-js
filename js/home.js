b270f754b7c1d9bbfa4f2a992983084e5eb2234b
umme-Tahazzee <tahazzeekaria1@gmail.com>
2025-02-23 13:02:07 +0600

    innertext, parseFloat, update balance in the UI

--- js/home.js
+++ js/home.js
@@ -1,13 +1,28 @@
 
 
-document.getElementById('btn-add-money').addEventListener('click', function(e){
-  e.preventDefault()
-  const addAmmount = document.getElementById('add-ammount').value;
-  const pinNumber = document.getElementById('pin').value
+document.getElementById('btn-add-money').addEventListener('click', function (e) {
+ e.preventDefault()
+ const addAmmount = document.getElementById('add-ammount').value;
+ const pinNumber = document.getElementById('pin').value
+
+ console.log(addAmmount, pinNumber,);
+ if (pinNumber === '1234') {
+  console.log('adding money to ur account');
+  //get the current balance
+  const balance = document.getElementById('account-balance').innerHTML
+
   
-  console.log(addAmmount, pinNumber, );
-  if(pinNumber !== '1234'){
-    alert('Something is wrong')
-  }
+  //add amount calculate
+  const addAmmountNumber = parseFloat(addAmmount)
+  const BalanceNumber = parseFloat(balance)
+  const currentBalance = (addAmmountNumber + BalanceNumber)
+  console.log(currentBalance);
   
+  //update balance in the UI
+   document.getElementById('account-balance').innerHTML = currentBalance
+
+ } else {
+  alert('something wrong please try again')
+ }
+
 })
